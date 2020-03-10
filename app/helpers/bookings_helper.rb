058415706365e22ba722d02f79ad18ea9a63b877
Bianca <miztrz@miz-ubuntu>
2020-03-10 20:26:40 +1100

    Added bookings in and out to dashboard

--- app/helpers/bookings_helper.rb
+++ app/helpers/bookings_helper.rb
@@ -3,4 +3,18 @@ module BookingsHelper
 		number_to_currency(booking.total)
 	end
 
+	def status(booking)
+		if booking.accepted? && booking.start_time > DateTime.now
+			return "accepted-pending"
+		elsif booking.accepted? && booking.start_time < DateTime.now
+			return "accepted-pastdate"
+		elsif !booking.accepted? && booking.start_time > DateTime.now
+			return "notaccepted-pending"
+		elsif !booking.accepted? && booking.start_time < DateTime.now
+			return "notaccepted-pastdate"
+		else
+			return "unknown"
+		end
+	end
+
 end
